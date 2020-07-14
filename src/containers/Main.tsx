48814d0fbd27e555cd2b1112f989c518ed6e1c01
pjg <pjg@whatvita.kr>
2020-07-14 09:00:54 +0900

    test redux 진행 중

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,23 +1,44 @@
 import React, { FunctionComponent } from 'react';
+import * as testActions from 'actions/Test';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import { GlobalNavHeader } from 'components/Headers';
 import classNames from 'classnames/bind';
 import styles from './Main.module.scss';
 
 const cx = classNames.bind(styles);
 
+interface Props {
+    dispatch: Dispatch;
+}
+
 /**
  * Route: /
  */
-const Main: FunctionComponent = () => {
+const Main: FunctionComponent<Props> = ({
+    dispatch,
+}) => {
+
+    const handleTest = () => {
+        dispatch(testActions.getTest());
+    };
 
     return (
         <div>
             <GlobalNavHeader />
             <div className={cx('container')}>
-                sdgds
+                <button onClick={handleTest}>
+                    버튼
+                </button>
             </div>
         </div>
     );
 }
 
-export default Main;
+const mapStateToProps = (state: any) => {
+    return {
+        user: state.user,
+    };
+};
+
+export default connect(mapStateToProps)(Main);
